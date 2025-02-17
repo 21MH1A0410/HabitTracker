02d89528b0b5112c87ab4adf5f840bc428ac6f5e
B Kodanda Ramaraju <bssvarma96@gmail.com>
2025-02-17 06:51:26 +0530

    new change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
+import store from "./store/store"; // Adjust the path as necessary
 import "react-toastify/dist/ReactToastify.css";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import store from "./store/store";
 import AddHabitForm from "./components/add-habit-form";
 import HabitList from "./components/habit-list";
 import HabitStats from "./components/habit-stats";
@@ -15,7 +13,6 @@ const App: React.FC = () => {
 
   return (
     <Provider store={store}>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
         <Container maxWidth="md">
           <Box sx={{ my: 4 }}>
             <Typography variant="h2" component="h1" gutterBottom align="center">
@@ -27,7 +24,6 @@ const App: React.FC = () => {
           </Box>
         </Container>
         <ToastContainer />
-      </LocalizationProvider>
     </Provider>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/HabitTracker/"> {/* Add basename here */}
+    <BrowserRouter > 
       <App />
     </BrowserRouter>
   </React.StrictMode>,
